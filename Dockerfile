89b4a1e51c8c812d87912360808595625f527e29
Fabio Tea <iam@f4b.io>
2021-01-27 23:16:41 +0100

    some updates

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,9 @@
-FROM node:14
+FROM node:latest
 COPY package.json .
 COPY package-lock.json .
 RUN npm ci
 COPY . .
 RUN npm run build
 
-FROM nginx:1.19-alpine
+FROM nginx:mainline-alpine
 COPY --from=0 ./build /usr/share/nginx/html
--- src/components/Header/Logo.tsx
+++ src/components/Header/Logo.tsx
@@ -1,12 +1,12 @@
-import React, { memo } from 'react'
-import './Logo.scss'
+import React, { memo } from 'react';
+import './Logo.scss';
 
 export const Logo = memo(function Logo() {
   return (
     <div className="logo">
       <span className="logo__shape">&lt;</span>
-      <span className="logo__char-1">Webdev</span>
-      <span className="logo__char-2">Home</span>
+      <span className="logo__char-1">dev</span>
+      <span className="logo__char-2">home</span>
       <span className="logo__shape"> /&gt;</span>
     </div>
   )
